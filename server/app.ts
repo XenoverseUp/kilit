fbcff0b777807f7f2c9ceaa74b8cb41b7d2d855b
XenoverseUp <xenoverseup@gmail.com>
2025-06-07 01:46:44 +0300

    rename: apps -> www

--- server/app.ts
+++ server/app.ts
@@ -5,13 +5,12 @@ import { logger } from "hono/logger";
 const app = new Hono();
 app.use("*", logger());
 
-// Test route
 app.get("/test", (c) => c.json({ success: true }));
 
-app.use("/dashboard/*", serveStatic({ root: "./apps/dashboard/dist" }));
-app.get("/dashboard/*", serveStatic({ path: "./apps/dashboard/dist/index.html" }));
+app.use("/dashboard/*", serveStatic({ root: "./www/dashboard/dist" }));
+app.get("/dashboard/*", serveStatic({ path: "./www/dashboard/dist/index.html" }));
 
-app.use("/*", serveStatic({ root: "./apps/landing/dist" }));
-app.get("/*", serveStatic({ path: "./apps/landing/dist/index.html" }));
+app.use("/*", serveStatic({ root: "./www/landing/dist" }));
+app.get("/*", serveStatic({ path: "./www/landing/dist/index.html" }));
 
 export default app;
