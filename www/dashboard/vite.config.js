59de29e0c07c4e615bfa699750873acc0357a729
XenoverseUp <xenoverseup@gmail.com>
2025-06-07 14:24:16 +0300

    feat: shared tailwind theme

--- www/dashboard/vite.config.js
+++ www/dashboard/vite.config.js
@@ -8,8 +8,8 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
     tsconfigPaths(),
+    TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
   ],
